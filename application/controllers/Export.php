2cc68f215dbdfcb0101a346701e3158298473d73
varun-garg <varun.10@live.com>
2015-08-22 11:52:03 +0000

    Fix export info

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -9,21 +9,21 @@
 class Export extends CI_Controller {
 
     function index() {
-        
-      
+
+
         $this->load->view('common/header_2');
         $this->load->view('blank');
-        $this->load->view('my_info');
+        $this->load->view('My_info');
         $this->load->view('blank');
-        $this->load->view('all_events');
+        $this->load->view('All_events');
         $this->load->view('blank');
-        $this->load->view('all_conferences');
+        $this->load->view('All_conferences');
         $this->load->view('blank');
-        $this->load->view('all_colleagues');
+        $this->load->view('All_colleagues');
         $this->load->view('blank');
-        $this->load->view('all_consultancy');
+        $this->load->view('All_consultancy');
         $this->load->view('common/footer_2');
-      
+        $this->load->view('blank');
     }
 
 }
