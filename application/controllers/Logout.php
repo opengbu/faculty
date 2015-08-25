fd7beeef430716e2110f546e7eb4e544f1898f62
varun-garg <varun.10@live.com>
2015-08-25 16:01:46 +0000

    Admin mode cant be logged out by going to some profile and logging out from there

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -1,13 +1,19 @@
 <?php
+
 /*
  *  Created on :Jul 10, 2015, 12:18:54 PM
  *  Author     :Varun Garg <varun.10@live.com>
  */
-class Logout extends CI_Controller
-{
-    function index()
-    {
+
+class Logout extends CI_Controller {
+
+    function index() {
+        $adm = 0;
+        if ($this->session->userdata('naac_admin') == 1)
+            $adm = 1;
         $this->session->sess_destroy();
+        $this->session->set_userdata('naac_admin', $adm);
         redirect(base_url());
     }
+
 }
